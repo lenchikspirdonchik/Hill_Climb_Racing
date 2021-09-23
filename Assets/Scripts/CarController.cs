d0ff72a935783c15fea3c65057c8a7770467732a
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-09-23 15:51:51 +0300

    cool shit

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,29 +6,57 @@ using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
-    public Rigidbody2D backTire, frontTire, carController;
-    public float speed = 80f, carTorque = 10f;
-    public float fuel = 5;
-    public float fuelСonsumption = 0.1f;
+    public Rigidbody2D carController;
+    public WheelJoint2D backTire, frontTire;
+    public float speed = 100000f, carTorque = 10f;
+    public float fuel = 1;
+    public float fuelСonsumption = 0.000005f;
     private float _movement = 0f;
     public Image fuelImage;
 
     void Update()
     {
-        _movement = Input.GetAxis("Horizontal") * -1;
-        carTorque = _movement * 10f;
+        // _movement = Input.GetAxis("Horizontal");
+        carTorque = _movement * -10f;
         fuelImage.fillAmount = fuel;
     }
 
     private void FixedUpdate()
     {
-        if (fuel > 0)
+        fuel -= fuelСonsumption;
+        if (_movement == 0f || fuel == 0f)
         {
-            backTire.AddTorque(_movement * speed * Time.deltaTime);
-            frontTire.AddTorque(_movement * speed * Time.deltaTime);
+            backTire.useMotor = false;
+            frontTire.useMotor = false;
+        }
+        else
+        {
+            backTire.useMotor = true;
+            frontTire.useMotor = true;
+            JointMotor2D motor = new JointMotor2D
+                { motorSpeed = _movement * speed * Time.deltaTime, maxMotorTorque = 10000 };
+            backTire.motor = motor;
+            frontTire.motor = motor;
             carController.AddTorque(_movement * carTorque * Time.fixedDeltaTime);
         }
-        fuel -= fuelСonsumption * Math.Abs(_movement) * Time.fixedDeltaTime;
-       
+    }
+
+    public void Acclerator(String pedal)
+    {
+        if (pedal == "Brake")
+        {
+            _movement = -1;
+        }
+        else
+        {
+            if (pedal == "Gas")
+            {
+                _movement = 1;
+            }
+            else
+            {
+                _movement = 0;
+            }
+        }
     }
 }
